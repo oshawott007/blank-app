326886b6e89d473c8ffab8a6fa122b0b26ab334b
oshawott007 <136514884+oshawott007@users.noreply.github.com>
2025-04-24 12:11:37 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,5 +3,5 @@ import streamlit as st
 st.title("🎈 My new app")
 st.write(
     "Let's start building! For help and inspiration, head over to [docs.streamlit.io](https://docs.streamlit.io/). hello world"
-    
+    "HOw are you"s
 )
