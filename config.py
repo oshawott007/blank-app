d48bc633d216921d177cec474ed582ab9f7e841d
oshawott007 <136514884+oshawott007@users.noreply.github.com>
2025-05-11 15:52:06 +0530

    Add files via upload

---
+++ config.py
@@ -0,0 +1,14 @@
+# # Configuration and constants
+# BOT_TOKEN = '7308989959:AAGGT-TeSfGO5lfHu2xhHmGaJ7soPftLn-E'
+# CHAT_DATA_FILE = "chat_data.json"
+# MONGO_URI = "mongodb://localhost:27017/"
+# DB_NAME = "occupancy_db"
+# COLLECTION_NAME = "occupancy_data"
+
+
+# config.py
+
+# Telegram Bot Configuration
+BOT_TOKEN = '7308989959:AAGGT-TeSfGO5lfHu2xhHmGaJ7soPftLn-E'
+CHAT_DATA_FILE = "chat_data.json"
+OCCUPANCY_DATA_FILE = "occupancy_data.json"
